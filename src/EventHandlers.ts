481a35c289faf8643b610504bb224734e3ef242c
Staxed <staxed@aeonforge.io>
2025-02-27 08:35:53 -0500

    undo previous commit updates, add to/from

--- src/EventHandlers.ts
+++ src/EventHandlers.ts
@@ -4,11 +4,7 @@ import { tryFetchIpfsFile } from "./utils/ipfs";
 const ZERO_ADDRESS = "0x0000000000000000000000000000000000000000";
 
 DragonZ.Transfer.handler(async ({ event, context }) => {
-  const timestamp = event.block.timestamp; // Get block timestamp
-  const fromAddress = event.params.from;
-  const toAddress = event.params.to;
-
-  if (fromAddress === ZERO_ADDRESS) {
+  if (event.params.from === ZERO_ADDRESS) {
     // mint
     let metadata = await tryFetchIpfsFile(
       event.params.tokenId.toString(),
@@ -17,11 +13,9 @@ DragonZ.Transfer.handler(async ({ event, context }) => {
 
     const nft: Nft = {
       id: event.params.tokenId.toString(),
-      owner: toAddress,
+      owner: event.params.to,
       image: metadata.image,
       attributes: JSON.stringify(metadata.attributes),
-      mintedAt: timestamp, // Store mint timestamp
-      transferHistory: JSON.stringify([{ from: ZERO_ADDRESS, to: toAddress, timestamp }]), // Initialize history
     };
     context.Nft.set(nft);
   } else {
@@ -30,19 +24,7 @@ DragonZ.Transfer.handler(async ({ event, context }) => {
     if (!nft) {
       throw new Error("Can't transfer non-existing NFT");
     }
-
-    // Update ownership
-    nft = { 
-      ...nft, 
-      owner: toAddress, 
-      lastTransferredAt: timestamp // Store last transfer timestamp
-    };
-
-    // Append transfer history
-    let history = nft.transferHistory ? JSON.parse(nft.transferHistory) : [];
-    history.push({ from: fromAddress, to: toAddress, timestamp });
-    nft.transferHistory = JSON.stringify(history);
-
+    nft = { ...nft, to: event.params.to, from: event.params.from };
     context.Nft.set(nft);
   }
 });
