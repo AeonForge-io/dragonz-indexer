566d2ce4306bf13e5337f402ec780240c57586fd
Staxed <staxed@aeonforge.io>
2025-02-27 16:19:54 -0500

    update new contract details

--- src/EventHandlers.ts
+++ src/EventHandlers.ts
@@ -1,4 +1,17 @@
-import { DragonZ, Nft } from "generated";
+import {
+  DragonZ,
+  DragonZ_Approval,
+  DragonZ_ApprovalForAll,
+  DragonZ_BatchMetadataUpdate,
+  DragonZ_MetadataUpdate,
+  DragonZ_OwnershipTransferred,
+  DragonZ_Transfer,
+  NetZFinance,
+  NetZFinance_Approval,
+  NetZFinance_AutoLiquify,
+  NetZFinance_OwnershipTransferred,
+  NetZFinance_Transfer,
+} from "generated";
 import { tryFetchIpfsFile } from "./utils/ipfs";
 
 const ZERO_ADDRESS = "0x0000000000000000000000000000000000000000";
@@ -34,4 +47,97 @@ DragonZ.Transfer.handler(async ({ event, context }) => {
     };
     context.Nft.set(nft);
   }
+});
+
+DragonZ.Approval.handler(async ({ event, context }) => {
+  const entity: DragonZ_Approval = {
+    id: `${event.chainId}_${event.block.number}_${event.logIndex}`,
+    owner: event.params.owner,
+    approved: event.params.approved,
+    tokenId: event.params.tokenId,
+  };
+
+  context.DragonZ_Approval.set(entity);
+});
+
+DragonZ.ApprovalForAll.handler(async ({ event, context }) => {
+  const entity: DragonZ_ApprovalForAll = {
+    id: `${event.chainId}_${event.block.number}_${event.logIndex}`,
+    owner: event.params.owner,
+    operator: event.params.operator,
+    approved: event.params.approved,
+  };
+
+  context.DragonZ_ApprovalForAll.set(entity);
+});
+
+DragonZ.BatchMetadataUpdate.handler(async ({ event, context }) => {
+  const entity: DragonZ_BatchMetadataUpdate = {
+    id: `${event.chainId}_${event.block.number}_${event.logIndex}`,
+    _fromTokenId: event.params._fromTokenId,
+    _toTokenId: event.params._toTokenId,
+  };
+
+  context.DragonZ_BatchMetadataUpdate.set(entity);
+});
+
+DragonZ.MetadataUpdate.handler(async ({ event, context }) => {
+  const entity: DragonZ_MetadataUpdate = {
+    id: `${event.chainId}_${event.block.number}_${event.logIndex}`,
+    _tokenId: event.params._tokenId,
+  };
+
+  context.DragonZ_MetadataUpdate.set(entity);
+});
+
+DragonZ.OwnershipTransferred.handler(async ({ event, context }) => {
+  const entity: DragonZ_OwnershipTransferred = {
+    id: `${event.chainId}_${event.block.number}_${event.logIndex}`,
+    previousOwner: event.params.previousOwner,
+    newOwner: event.params.newOwner,
+  };
+
+  context.DragonZ_OwnershipTransferred.set(entity);
+});
+
+NetZFinance.Approval.handler(async ({ event, context }) => {
+  const entity: NetZFinance_Approval = {
+    id: `${event.chainId}_${event.block.number}_${event.logIndex}`,
+    owner: event.params.owner,
+    spender: event.params.spender,
+    value: event.params.value,
+  };
+
+  context.NetZFinance_Approval.set(entity);
+});
+
+NetZFinance.AutoLiquify.handler(async ({ event, context }) => {
+  const entity: NetZFinance_AutoLiquify = {
+    id: `${event.chainId}_${event.block.number}_${event.logIndex}`,
+    amountNETZ: event.params.amountNETZ,
+    amountBOG: event.params.amountBOG,
+  };
+
+  context.NetZFinance_AutoLiquify.set(entity);
+});
+
+NetZFinance.OwnershipTransferred.handler(async ({ event, context }) => {
+  const entity: NetZFinance_OwnershipTransferred = {
+    id: `${event.chainId}_${event.block.number}_${event.logIndex}`,
+    previousOwner: event.params.previousOwner,
+    newOwner: event.params.newOwner,
+  };
+
+  context.NetZFinance_OwnershipTransferred.set(entity);
+});
+
+NetZFinance.Transfer.handler(async ({ event, context }) => {
+  const entity: NetZFinance_Transfer = {
+    id: `${event.chainId}_${event.block.number}_${event.logIndex}`,
+    from: event.params.from,
+    to: event.params.to,
+    value: event.params.value,
+  };
+
+  context.NetZFinance_Transfer.set(entity);
 });
